c3bd0f82fc783779246523c2bf032f39a6dbbb90
taylor <tayjaybabee@gmail.com>
2020-06-01 02:58:53 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+        name='gitlab-installer',
+        version='0.1.0',
+        packages=[ 'tests', 'gitlab_installer' ],
+        url='https://github.com/Inspyre-Softworks/gitlab-installer',
+        license='MIT',
+        author='Taylor-Jayde Jasmine Blackstone',
+        author_email='t.blackstone@inspyre.tech',
+        description='Description here'
+)
